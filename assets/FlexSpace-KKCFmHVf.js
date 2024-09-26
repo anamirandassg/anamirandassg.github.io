96bce6e895d8ed189dd8d14553daf36b490e7370
Hector Ricardo <hectoricardom@gmail.com>
2024-09-26 08:54:08 -0400

    kmnubyvtcrx45c6v7b8n

---
+++ assets/FlexSpace-KKCFmHVf.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-ClNVsa9Q.js";var t=e("<div class=flexSpace>");const l=()=>t();export{l as default};
---
+++ assets/isMobile-BkMSgOMa.js
@@ -0,0 +1,1 @@
+import{p as o}from"./index-ClNVsa9Q.js";function l(i){let t,e;function n(){t=setInterval(()=>{e!==window.outerWidth>460&&(e=window.outerWidth>460,i(e))},450)}return o(()=>clearInterval(t)),n}export{l as u};
