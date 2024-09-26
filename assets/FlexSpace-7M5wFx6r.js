958db0393595bb06524d2c320877250c2def927b
Hector Ricardo <hectoricardom@gmail.com>
2024-09-26 05:32:20 -0400

    nbyt6b7h8

--- assets/FlexSpace-7M5wFx6r.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-BdPh1Ntw.js";var t=e("<div class=flexSpace>");const p=()=>t();export{p as F};
---
+++ assets/isMobile-CQ6rABsX.js
@@ -0,0 +1,1 @@
+import{p as o}from"./index-D-Eshf04.js";function l(i){let t,e;function n(){t=setInterval(()=>{e!==window.outerWidth>460&&(e=window.outerWidth>460,i(e))},450)}return o(()=>clearInterval(t)),n}export{l as u};
