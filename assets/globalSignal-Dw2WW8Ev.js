485bcfe673d707081447a4fca29a886276d14183
Hector Ricardo <hectoricardom@gmail.com>
2024-11-06 20:46:42 -0500

    njhbvct567g8h

---
+++ assets/globalSignal-Dw2WW8Ev.js
@@ -0,0 +1,1 @@
+import{a as e,c as o}from"./index-DmFmaTv5.js";const[i,s]=e(!0),[n,l]=e(!0);setInterval(()=>{let t=720;i()!==window.outerWidth<t&&s(window.outerWidth<t);let a=1080;n()!==window.outerWidth<a&&l(window.outerWidth<a)},220);const[c,d]=o({});o({});export{n as a,c as g,i,d as s};
