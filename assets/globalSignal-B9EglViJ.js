fe1f8a9f40826d09d2c0d2c6d76744878cc10c7f
Hector Ricardo <hectoricardom@gmail.com>
2024-11-16 09:43:33 -0500

    nubgvycx56v7g8h

---
+++ assets/globalSignal-B9EglViJ.js
@@ -0,0 +1,1 @@
+import{a as s,c as t}from"./index-dnI_iE0A.js";const[i,o]=s(!0),[n,l]=s(!0);setInterval(()=>{let a=720;i()!==window.outerWidth<a&&o(window.outerWidth<a);let e=1080;n()!==window.outerWidth<e&&l(window.outerWidth<e)},220);const[c,b]=t({});t({});const[d,w]=t({});export{w as a,n as b,c as g,i,d as m,b as s};
